2364a3d82bcb13d86c5f4d67ab0eda010de1cf06
johanley <johnohanley85@gmail.com>
2024-07-09 19:47:39 -0300

    Review speeds dir.

--- src/sr/explore/speeds/SpeedsAndGammas.java
+++ src/sr/explore/speeds/SpeedsAndGammas.java
@@ -14,9 +14,13 @@ public class SpeedsAndGammas {
   
   public static void main(String... args) {
     List<String> lines = new ArrayList<>();
-    lines.add("# β,  Γ");
+    lines.add("β,  Γ");
+    lines.add(Util.separator(25));
     for(Speed speed : Speed.values()) {
       lines.add(speed.βBigDecimal() + " " + speed.Γ());
+    }
+    for(String line : lines) {
+      System.out.println(line);
     }
     Util.writeToFile(SpeedsAndGammas.class, "speeds-and-gammas.txt", lines);
   }
