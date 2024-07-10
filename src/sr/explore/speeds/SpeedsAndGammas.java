7d2ba4c5ac0ba35dfe2de38707c815942231b633
johanley <johnohanley85@gmail.com>
2024-07-10 09:38:42 -0300

    Tabular output for speeds.

--- src/sr/explore/speeds/SpeedsAndGammas.java
+++ src/sr/explore/speeds/SpeedsAndGammas.java
@@ -1,3 +1,4 @@
+
 package sr.explore.speeds;
 
 import java.util.ArrayList;
@@ -14,15 +15,21 @@ public class SpeedsAndGammas {
   
   public static void main(String... args) {
     List<String> lines = new ArrayList<>();
-    lines.add("β,  Γ");
-    lines.add(Util.separator(25));
+    lines.add("Lorentz factor Γ as a function of speed β=v/c:" + Util.NL);
+    lines.add(column(1,"β") + column(2,"Γ"));
+    lines.add(Util.separator(50));
     for(Speed speed : Speed.values()) {
-      lines.add(speed.βBigDecimal() + " " + speed.Γ());
+      lines.add(column(1, speed.βBigDecimal()) + column(2, speed.Γ()));
     }
     for(String line : lines) {
       System.out.println(line);
     }
     Util.writeToFile(SpeedsAndGammas.class, "speeds-and-gammas.txt", lines);
   }
-
+  
+  private static final String[] COLS = {"%-32s", "%-20s"};
+  
+  private static String column(int idx, Object thing) {
+    return String.format(COLS[idx-1], thing);
+  }
 }
