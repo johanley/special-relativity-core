17c30d07fd0894fc76947aa99bc1a359ed9d03f3
johanley <johnohanley85@gmail.com>
2024-07-10 11:46:38 -0300

    Centralize some output policies.

--- src/sr/explore/speeds/SpeedsAndGammas.java
+++ src/sr/explore/speeds/SpeedsAndGammas.java
@@ -1,31 +1,31 @@
 
 package sr.explore.speeds;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import sr.core.Speed;
 import sr.core.Util;
 import sr.explore.Table;
+import sr.explore.TextOutput;
 
 /** 
  Simply list speeds and their corresponding Lorentz factors.
  Includes the speed of extreme cosmic rays. 
 */
-public class SpeedsAndGammas {
+public class SpeedsAndGammas extends TextOutput {
   
   public static void main(String... args) {
-    Table table = new Table("%-32s", "%-20s");
-    List<String> lines = new ArrayList<>();
-    lines.add("Lorentz factor Γ as a function of speed β=v/c:" + Util.NL);
+    SpeedsAndGammas speeds = new SpeedsAndGammas();
+    speeds.execute();
+  }
+  
+  void execute() {
+    lines.add("The Lorentz factor Γ as a function of speed β=v/c:" + Util.NL);
     lines.add(table.row("β", "Γ"));
     lines.add(Util.separator(50));
     for(Speed speed : Speed.values()) {
       lines.add(table.row(speed.βBigDecimal(), speed.Γ()));
     }
-    for(String line : lines) {
-      System.out.println(line);
-    }
-    Util.writeToFile(SpeedsAndGammas.class, "speeds-and-gammas.txt", lines);
+    outputLines("speeds-and-gammas.txt");
   }
+  
+  private Table table = new Table("%-32s", "%-20s");
 }
--- src/sr/explore/traveltime/TravelTime.java
+++ src/sr/explore/traveltime/TravelTime.java
@@ -1,21 +1,20 @@
 package sr.explore.traveltime;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import sr.core.Speed;
 import sr.core.Util;
 import sr.explore.Table;
+import sr.explore.TextOutput;
 
 /** 
  Travel-time (proper time) for various trips, according to the traveler. 
 */
-public final class TravelTime {
+public final class TravelTime extends TextOutput {
   
   public static void main(String... args) {
     TravelTime tt = new TravelTime();
-    tt.oneWayTripUniformVelocity(50.0);
-    tt.twoWayTripUniformSpeed(50.0);
+    double lightyears = 50.0;
+    tt.oneWayTripUniformVelocity(lightyears);
+    tt.twoWayTripUniformSpeed(lightyears);
   }
   
   /** 
@@ -24,7 +23,6 @@ public final class TravelTime {
    according to their own clock. 
   */
   void oneWayTripUniformVelocity(double lightyears) {
-    List<String> lines = new ArrayList<>();
     lines.add("One-way trip at uniform velocity, traveling a distance of " + lightyears + " light-years.");
     lines.add("Travel-time is proper time.");
     lines.add("Acts like a wormhole, from the point of view of the traveler." + Util.NL);
@@ -37,9 +35,8 @@ public final class TravelTime {
         lines.add(table.row(speed.β(), round(travelTime), round(coordTime)));
       }
     }
-    output(lines, "travel-time-one-way-" + lightyears + ".txt");
+    outputLines("travel-time-one-way-" + lightyears + ".txt");
   }
-  
 
   /** 
    A two-way trip.
@@ -48,8 +45,7 @@ public final class TravelTime {
    A two-way trip acts like a time machine into the future: the traveler gets into the deep future faster than usual.
   */
   void twoWayTripUniformSpeed(double lightyears) {
-    List<String> lines = new ArrayList<>();
-    lines.add(Util.NL + "Round trip, same speed on both outbound and inbound, traveling out " + lightyears + " light years and back.");
+    lines.add("Round trip, same speed on both outbound and inbound, traveling out " + lightyears + " light years and back.");
     lines.add("Travel-time is proper time.");
     lines.add("For the traveler, this acts like a time machine into the future." + Util.NL);
     lines.add(table.row("β speed", "travel-time (y)", "coord-time (y)"));
@@ -61,18 +57,11 @@ public final class TravelTime {
         lines.add(table.row(speed.β(), round(2.0*travelTime), round(2.0*coordTime)));
       }
     }
-    output(lines, "travel-time-two-way-" + lightyears + ".txt");
+    outputLines("travel-time-two-way-" + lightyears + ".txt");
   }
   
   private Table table = new Table("%-20s", "%-20s", "%-20s");
 
-  private void output(List<String> lines, String fileName) {
-    for(String line : lines) {
-      System.out.println(line);
-    }
-    Util.writeToFile(this.getClass(), fileName, lines);
-  }
-  
   private double round(Double value) {
     return Util.round(value, 4);
   }
