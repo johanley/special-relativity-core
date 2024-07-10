f0aa5bdb0cc05446043077fd8ef9f0251b6d2ebc
johanley <johnohanley85@gmail.com>
2024-07-10 09:57:54 -0300

    Table output as text.

---
+++ src/sr/explore/Table.java
@@ -0,0 +1,30 @@
+package sr.explore;
+
+import java.util.Formatter;
+
+/** Output text in a tabular format. */
+public final class Table {
+
+  /**
+   Constructor.
+   @param columFormats as defined by the {@link Formatter} class. 
+  */
+  public Table(String... columnFormats) {
+    this.columnFormats = columnFormats;
+  }
+
+  /** Return a formatted row of items in the table. */
+  public String row(Object... values) {
+    StringBuilder result = new StringBuilder();
+    for(int idx=0; idx < columnFormats.length; ++idx) {
+      result.append(column(idx, values[idx]));
+    }
+    return result.toString();
+  }
+  
+  private String[] columnFormats;
+  
+  private String column(int idx, Object thing) {
+    return String.format(columnFormats[idx], thing);
+  }
+}
