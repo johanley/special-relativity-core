4f2d2e58ce459edc8a618d8cd90db1e38869bca9
johanley <johnohanley85@gmail.com>
2024-10-14 15:13:23 -0300

    Issue #104

--- src/sr/explore/accel/elbow/boost/kinematic/rotation/KinematicRotationRange.java
+++ src/sr/explore/accel/elbow/boost/kinematic/rotation/KinematicRotationRange.java
@@ -1,20 +1,22 @@
 package sr.explore.accel.elbow.boost.kinematic.rotation;
 
 import sr.core.Axis;
+import sr.core.KinematicRotation;
 import sr.core.SpeedValues;
 import sr.core.Util;
+import sr.core.vec3.Velocity;
 import sr.explore.Exploration;
-import sr.explore.accel.elbow.boost.EquivalentBoostPlusRotation;
 import sr.output.text.Table;
 import sr.output.text.TextOutput;
 
 /** 
  Explore the range of values of the kinematic rotation angle (Wigner rotation angle). 
  
- <P>In this class, a corner-boost is used and the extreme max angle is pi/2.
+ <P>In this class, a corner-boost is used and the extreme max angle is π/2.
  
- <P>In general, the extreme max angle is pi. 
+ <P>In general, the extreme max angle is π. 
  This is approached when the velocities are in nearly opposite directions, and both speeds are ultra-relativistic.
+ In hyperbolic geometry, this corresponds to maximum <em>angular defect</em> of a triangle. 
 */
 public final class KinematicRotationRange extends TextOutput implements Exploration {
 
@@ -25,9 +27,9 @@ public final class KinematicRotationRange extends TextOutput implements Explorat
   
   @Override public void explore() {
     add("Kinematic (Wigner) rotation (θw) for various combined speeds.");
-    add("Here, the pair of velocities are at always at right angles to each other.");
+    add("Here, the pair of velocities are always at right angles to each other.");
     add("");
-    add(tableHeader.row("β1", "β2", "θw"));
+    add(tableHeader.row("β1", "β2", "|θw|"));
     add(dashes(55));
     showRangeθw();
     outputToConsoleAnd("kinematic-rotation-range.txt");
@@ -36,16 +38,17 @@ public final class KinematicRotationRange extends TextOutput implements Explorat
   void showRangeθw() {
     for (SpeedValues β1 : SpeedValues.nonExtremeValues()) {
       for (SpeedValues β2 : SpeedValues.nonExtremeValues()) {
-        EquivalentBoostPlusRotation range = new EquivalentBoostPlusRotation(Axis.Z, β1.β(), β2.β());
-        Double θw = range.equivalent().θw;
-        add(table.row(β1.β(), β2.β(), round(Util.radsToDegs(θw))));
+        Velocity veloOne = Velocity.of(β1.β(), Axis.X);
+        Velocity veloTwo = Velocity.of(β2.β(), Axis.Y);
+        KinematicRotation kr = KinematicRotation.of(veloOne, veloTwo);
+        add(table.row(β1.β(), β2.β(), round(Util.radsToDegs(kr.θw()))));
       }
     }
   }
   
   // β1, β2, θw
   private Table table = new Table("%-21s", "%-21s", "%8.2f°");
-  private Table tableHeader = new Table("%-21s", "%-21s", "%6s°");
+  private Table tableHeader = new Table("%-21s", "%-23s", "%6s°");
   
   private double round(double value) {
     return Util.round(value, 2);
